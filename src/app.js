56953c7e474094d9d30be621a21ba5ca9daedc4a
Adjani <133295232+Adjani09@users.noreply.github.com>
2023-07-27 22:05:07 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+const domain = () => {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        console.log(pronoun[i] + adj[a] + noun[n] + ".com");
+      }
+    }
+  }
+};
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("hola martin");
+  domain();
 };
